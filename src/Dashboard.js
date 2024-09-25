b3b2489d7b3e3138bb66d3f3a4af14995779e35e
vishnu563 <vishnuprasanth050603@gmail.com>
2024-09-25 18:25:35 +0530

    All Components Aligned

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function Dashboard() {
   const [employees, setEmployees] = useState(() => {
-    const savedEmployees = localStorage.getItem('employees');
+    const savedEmployees = localStorage.getItem("employees");
     return savedEmployees ? JSON.parse(savedEmployees) : [];
   });
   const [showForm, setShowForm] = useState(false);
   const [newEmployee, setNewEmployee] = useState({
-    id: '',
-    name: '',
-    email: '',
-    mobile: '',
-    designation: '',
-    gender: '',
+    id: "",
+    name: "",
+    email: "",
+    mobile: "",
+    designation: "",
+    gender: "",
     course: [],
-    createDate: new Date().toLocaleDateString()
+    createDate: new Date().toLocaleDateString(),
   });
 
   const [isEditing, setIsEditing] = useState(null);
@@ -24,228 +24,248 @@ function Dashboard() {
   const username = location.state?.username || "User";
 
   useEffect(() => {
-    localStorage.setItem('employees', JSON.stringify(employees));
+    localStorage.setItem("employees", JSON.stringify(employees));
   }, [employees]);
 
   function handleInputChange(e) {
     const { name, value, type, checked, files } = e.target;
 
-    if (type === 'file') {
+    if (type === "file") {
       setNewEmployee({ ...newEmployee, [name]: files[0] });
-    } 
-    else if (type === 'checkbox') {
+    } else if (type === "checkbox") {
       if (checked) {
-        setNewEmployee(prevState => ({
+        setNewEmployee((prevState) => ({
           ...prevState,
-          course: [...prevState.course, value]
+          course: [...prevState.course, value],
+        }));
+      } else {
+        setNewEmployee((prevState) => ({
+          ...prevState,
+          course: prevState.course.filter((course) => course !== value),
         }));
       }
-      else {
-        setNewEmployee(prevState => ({
-          ...prevState,
-          course: prevState.course.filter(course => course !== value)
-        }));
-      }
-      } 
-      else {
-        setNewEmployee({ ...newEmployee, [name]: value });
-      }
+    } else {
+      setNewEmployee({ ...newEmployee, [name]: value });
     }
+  }
 
   function handleFormSubmit(e) {
     e.preventDefault();
 
     if (newEmployee.course.length === 0) {
-      alert('Please select at least one course');
+      alert("Please select at least one course");
       return;
     }
     if (newEmployee.name && newEmployee.email) {
       if (isEditing !== null) {
-        const updatedEmployees = employees.map(employee => 
+        const updatedEmployees = employees.map((employee) =>
           employee.id === isEditing ? { ...employee, ...newEmployee } : employee
         );
         setEmployees(updatedEmployees);
         setIsEditing(null);
-        } 
-      else {
-        const nextId = employees.length > 0 ? Math.max(...employees.map(emp => emp.id)) + 1 : 1;
+      } else {
+        const nextId =
+          employees.length > 0
+            ? Math.max(...employees.map((emp) => emp.id)) + 1
+            : 1;
         setEmployees([...employees, { ...newEmployee, id: nextId }]);
       }
       setNewEmployee({
-        id: '',
-        name: '',
-        email: '',
-        mobile: '',
-        designation: '',
-        gender: '',
+        id: "",
+        name: "",
+        email: "",
+        mobile: "",
+        designation: "",
+        gender: "",
         course: [],
-        createDate: new Date().toLocaleDateString()
+        createDate: new Date().toLocaleDateString(),
       });
       setShowForm(false);
-      } 
-      else {
-        alert('Please fill all required fields');
-      }
+    } else {
+      alert("Please fill all required fields");
     }
+  }
 
-    function addEmployee() {
-      setShowForm(true);
-    }
+  function addEmployee() {
+    setShowForm(true);
+  }
 
-    function handleLogout() {
-      const confirmLogout = window.confirm("Are you sure you want to log out?");
-      if (confirmLogout) {
-        navigate('/');
-      }
+  function handleLogout() {
+    const confirmLogout = window.confirm("Are you sure you want to log out?");
+    if (confirmLogout) {
+      navigate("/");
     }
+  }
 
-    function handleDelete(id) {
-      const updatedEmployees = employees.filter(employee => employee.id !== id);
-      setEmployees(updatedEmployees);
-    }
-  
+  function handleDelete(id) {
+    const updatedEmployees = employees.filter((employee) => employee.id !== id);
+    setEmployees(updatedEmployees);
+  }
 
-    function handleEdit(employee) {
-      setNewEmployee(employee);
-      setIsEditing(employee.id);
-      setShowForm(true);
-    }
-    function handleCancel() {
-      setNewEmployee({
-        id: '',
-        name: '',
-        email: '',
-        mobile: '',
-        designation: '',
-        gender: '',
-        course: [],
-        createDate: new Date().toLocaleDateString()
-      });
-      setShowForm(false);
-      setIsEditing(null);
-    }
+  function handleEdit(employee) {
+    setNewEmployee(employee);
+    setIsEditing(employee.id);
+    setShowForm(true);
+  }
+  function handleCancel() {
+    setNewEmployee({
+      id: "",
+      name: "",
+      email: "",
+      mobile: "",
+      designation: "",
+      gender: "",
+      course: [],
+      createDate: new Date().toLocaleDateString(),
+    });
+    setShowForm(false);
+    setIsEditing(null);
+  }
 
   return (
     <>
-      <header className='head'>
+      <header className="head">
         <h2>Dash Board</h2>
-        <nav className='navBar'>
-          <ul className='navBar-list'>
+        <nav className="navBar">
+          <ul className="navBar-list">
             <li>Home</li>
             <li>Employee List</li>
             <li>{username}</li>
-            <li className='logout' onClick={handleLogout}>Logout</li>
+            <li className="logout" onClick={handleLogout}>
+              Logout
+            </li>
           </ul>
         </nav>
-        <div className='count'>{!showForm && <p className='count'>Total Count : {employees.length}</p>}</div>
+        <div className="count">
+          {!showForm && (
+            <p className="count">Total Count : {employees.length}</p>
+          )}
+        </div>
       </header>
 
       <main>
-        <div><button className='createButton' onClick={addEmployee}>Create Employee +</button></div>
+        <div>
+          <button className="createButton" onClick={addEmployee}>
+            Create Employee +
+          </button>
+        </div>
         {!showForm && employees.length === 0 ? (
-          <h2 className='adminHead'>Welcome Admin Panel!</h2>
+          <h2 className="adminHead">Welcome Admin Panel!</h2>
         ) : (
           <div>
             {showForm ? (
-                <div className="employee-form">
-                  <h3 className='employee-head'>{isEditing ? "Edit Employee" : "Add New Employee"}</h3>
-                  <form onSubmit={handleFormSubmit}>
-                    <input
-                      type="text"
-                      name="name"
-                      placeholder="Name"
-                      value={newEmployee.name}
-                      onChange={handleInputChange}
-                      required
-                    />
-                    <input
-                      type="email"
-                      name="email"
-                      placeholder="Email"
-                      value={newEmployee.email}
-                      onChange={handleInputChange}
-                      required
-                    />
-                    <input
-                      type="tel"
-                      name="mobile"
-                      placeholder="Mobile"
-                      value={newEmployee.mobile}
-                      onChange={handleInputChange}
-                      maxLength={10}
-                      pattern="\d*"
-                      required
-                    />
+              <div className="employee-form">
+                <h3 className="employee-head">
+                  {isEditing ? "Edit Employee" : "Add New Employee"}
+                </h3>
+                <form onSubmit={handleFormSubmit}>
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    value={newEmployee.name}
+                    onChange={handleInputChange}
+                    required
+                  />
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={newEmployee.email}
+                    onChange={handleInputChange}
+                    required
+                  />
+                  <input
+                    type="tel"
+                    name="mobile"
+                    placeholder="Mobile"
+                    value={newEmployee.mobile}
+                    onChange={handleInputChange}
+                    maxLength={10}
+                    pattern="\d*"
+                    required
+                  />
 
-                    <div className='designation'>
-                      <select name="designation" value={newEmployee.designation} onChange={handleInputChange} required>
-                        <option value="" disabled>Designation</option>
-                        <option value="HR">HR</option>
-                        <option value="Sales">Sales</option>
-                        <option value="Manager">Manager</option>
-                      </select>
-                    </div>
-                    <label className='gender'>
+                  <div className="designation">
+                    <select
+                      name="designation"
+                      value={newEmployee.designation}
+                      onChange={handleInputChange}
+                      required
+                    >
+                      <option value="" disabled>
+                        Designation
+                      </option>
+                      <option value="HR">HR</option>
+                      <option value="Sales">Sales</option>
+                      <option value="Manager">Manager</option>
+                    </select>
+                  </div>
+                  <label className="gender">
+                    <input
+                      type="radio"
+                      name="gender"
+                      value="Male"
+                      checked={newEmployee.gender === "Male"}
+                      onChange={handleInputChange}
+                      required
+                    />
+                    Male
+                    <input
+                      type="radio"
+                      name="gender"
+                      value="Female"
+                      checked={newEmployee.gender === "Female"}
+                      onChange={handleInputChange}
+                      required
+                    />
+                    Female
+                  </label>
+                  <div className="course" required>
+                    <label>
                       <input
-                        type="radio"
-                        name="gender"
-                        value="Male"
-                        checked={newEmployee.gender === 'Male'}
+                        type="checkbox"
+                        name="course"
+                        value="MCA"
+                        checked={newEmployee.course.includes("MCA")}
                         onChange={handleInputChange}
-                        required
                       />
-                      Male
-                      <input
-                        type="radio"
-                        name="gender"
-                        value="Female"
-                        checked={newEmployee.gender === 'Female'}
-                        onChange={handleInputChange}
-                        required
-                      />
-                      Female
+                      MCA
                     </label>
-                    <div className='course' required>
-                      <label>
-                        <input
-                          type="checkbox"
-                          name="course"
-                          value="MCA"
-                          checked={newEmployee.course.includes('MCA')}
-                          onChange={handleInputChange}
-                        />
-                        MCA
-                      </label>
-                      <label>
-                        <input
-                          type="checkbox"
-                          name="course"
-                          value="BCA"
-                          checked={newEmployee.course.includes('BCA')}
-                          onChange={handleInputChange}
-                        />
-                        BCA
-                      </label>
-                      <label>
-                        <input
-                          type="checkbox"
-                          name="course"
-                          value="BSC"
-                          checked={newEmployee.course.includes('BSC')}
-                          onChange={handleInputChange}
-                        />
-                        BSC
-                      </label>
-                    </div>
-                    <div>
-                    <button type="submit">{isEditing ? "Update" : "Submit"}</button>
-                    <button type="button" onClick={handleCancel}>Cancel</button>
-                    </div> 
-                  </form>
-                </div>
+                    <label>
+                      <input
+                        type="checkbox"
+                        name="course"
+                        value="BCA"
+                        checked={newEmployee.course.includes("BCA")}
+                        onChange={handleInputChange}
+                      />
+                      BCA
+                    </label>
+                    <label>
+                      <input
+                        type="checkbox"
+                        name="course"
+                        value="BSC"
+                        checked={newEmployee.course.includes("BSC")}
+                        onChange={handleInputChange}
+                      />
+                      BSC
+                    </label>
+                  </div>
+                  <div>
+                    <button type="submit">
+                      {isEditing ? "Update" : "Submit"}
+                    </button>
+                    <button type="button" onClick={handleCancel}>
+                      Cancel
+                    </button>
+                  </div>
+                </form>
+              </div>
             ) : (
               employees.length > 0 && (
-                <table className='employee-table'>
+                <table className="employee-table">
                   <thead>
                     <tr>
                       <th>Employee Id</th>
@@ -268,13 +288,17 @@ function Dashboard() {
                         <td>{employee.mobile}</td>
                         <td>{employee.designation}</td>
                         <td>{employee.gender}</td>
-                        <td>{employee.course.join(', ')}</td>
+                        <td>{employee.course.join(", ")}</td>
                         <td>{employee.createDate}</td>
                         <td>
-                          <button onClick={() => handleEdit(employee)}>Edit</button>
+                          <button onClick={() => handleEdit(employee)}>
+                            Edit
+                          </button>
                         </td>
                         <td>
-                          <button onClick={() => handleDelete(employee.id)}>Delete</button>
+                          <button onClick={() => handleDelete(employee.id)}>
+                            Delete
+                          </button>
                         </td>
                       </tr>
                     ))}
--- src/Login.js
+++ src/Login.js
@@ -1,117 +1,133 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-    const [signUpUsername, setSignUpUsername] = useState("");
-    const [signUpEmail, setSignUpEmail] = useState("");
-    const [signUpPassword, setSignUpPassword] = useState("");
-    const [signinUsername, setSigninUsername] = useState("");
-    const [signinPassword, setSigninPassword] = useState("");
-    const [isSignUp, setIsSignUp] = useState(false);
-    const navigate = useNavigate();
+  const [signUpUsername, setSignUpUsername] = useState("");
+  const [signUpEmail, setSignUpEmail] = useState("");
+  const [signUpPassword, setSignUpPassword] = useState("");
+  const [signinUsername, setSigninUsername] = useState("");
+  const [signinPassword, setSigninPassword] = useState("");
+  const [isSignUp, setIsSignUp] = useState(false);
+  const navigate = useNavigate();
 
-    function handleSignUpSubmit(e) {
-        e.preventDefault();
-        if (signUpUsername&& signUpPassword && signUpEmail) {
-            
-        } else {
-            alert("Please fill out all fields for Sign Up.");
-        }
+  function handleSignUpSubmit(e) {
+    e.preventDefault();
+    if (signUpUsername && signUpPassword && signUpEmail) {
+    } else {
+      alert("Please fill out all fields for Sign Up.");
     }
+  }
 
-    function handleSignInSubmit(e) {
-        e.preventDefault();
-        if (signinUsername && signinPassword) {
-            navigate('/dashboard', { state: { username: signinUsername } });
-        } else {
-            alert("Please enter both email and password.");
-        }
+  function handleSignInSubmit(e) {
+    e.preventDefault();
+    if (signinUsername && signinPassword) {
+      navigate("/dashboard", { state: { username: signinUsername } });
+    } else {
+      alert("Please enter both email and password.");
     }
+  }
 
-    return (
-        <div className={`container ${isSignUp ? "right-panel-active" : ""}`} id="main">
-            <main>
-                <section className="sign-up">
-                    <form onSubmit={handleSignUpSubmit}>
-                        <h1>Create Account</h1>
-                        <input
-                            type="text"
-                            id="signUpUsername"
-                            name="signUpUsername"
-                            placeholder='User Name'
-                            value={signUpUsername}
-                            onChange={(e) => setSignUpUsername(e.target.value)}
-                            minLength={6}
-                            required
-                        />
-        
-                        <input
-                            type="email"
-                            id="signUpEmail"
-                            name="signUpEmail"
-                            placeholder='Email'
-                            value={signUpEmail}
-                            onChange={(e) => setSignUpEmail(e.target.value)}
-                            required
-                        />
-                     
-                        <input
-                            type="password"
-                            id="signUpPassword"
-                            name="signUpPassword"
-                            placeholder='Password'
-                            value={signUpPassword}
-                            onChange={(e) => setSignUpPassword(e.target.value)}
-                            minLength={8}
-                            required
-                        />
-                        <button id='signup-button' type="submit">Signup</button>
-                    </form>
-                </section>
+  return (
+    <div
+      className={`container ${isSignUp ? "right-panel-active" : ""}`}
+      id="main"
+    >
+      <main>
+        <section className="sign-up">
+          <form onSubmit={handleSignUpSubmit}>
+            <h1>Create Account</h1>
+            <input
+              type="text"
+              id="signUpUsername"
+              name="signUpUsername"
+              placeholder="User Name"
+              value={signUpUsername}
+              onChange={(e) => setSignUpUsername(e.target.value)}
+              minLength={6}
+              required
+            />
 
-                <section className="sign-in">
-                    <form onSubmit={handleSignInSubmit}>
-                        <h1>Sign In</h1>
-                        <input
-                            type="text"
-                            id="signinUsername"
-                            name="signinUsername"
-                            placeholder='User Name'
-                            value={signinUsername}
-                            onChange={(e) => setSigninUsername(e.target.value)}
-                            required
-                        />
-                        <input
-                            type="password"
-                            id="signinpassword"
-                            name="signinpassword"
-                            placeholder='Password'
-                            value={signinPassword}
-                            onChange={(e) => setSigninPassword(e.target.value)}
-                            minLength={8}
-                            required
-                        />
-                        <button id='signin-button' type="submit">SignIn</button>
-                    </form>
-                </section>
+            <input
+              type="email"
+              id="signUpEmail"
+              name="signUpEmail"
+              placeholder="Email"
+              value={signUpEmail}
+              onChange={(e) => setSignUpEmail(e.target.value)}
+              required
+            />
 
-                <section className="overlay-container">
-                    <div className="overlay">
-                        <div className="overlay-left">
-                            <h1>Welcome Back!</h1>
-                            <p>Log in to access the admin dashboard and manage your employee data efficiently.</p>
-                            <button onClick={() => setIsSignUp(false)} id="signIn">Sign In</button>
-                        </div>
-                        <div className="overlay-right">
-                            <h1>Hello!</h1>
-                            <p>Join the admin community and start building efficient employee management systems today.</p>
-                            <button onClick={() => setIsSignUp(true)} id="signUp">Sign Up</button>
-                        </div>
-                    </div>
-                </section>
-            </main>
-        </div>
-    );
+            <input
+              type="password"
+              id="signUpPassword"
+              name="signUpPassword"
+              placeholder="Password"
+              value={signUpPassword}
+              onChange={(e) => setSignUpPassword(e.target.value)}
+              minLength={8}
+              required
+            />
+            <button id="signup-button" type="submit">
+              Signup
+            </button>
+          </form>
+        </section>
+
+        <section className="sign-in">
+          <form onSubmit={handleSignInSubmit}>
+            <h1>Sign In</h1>
+            <input
+              type="text"
+              id="signinUsername"
+              name="signinUsername"
+              placeholder="User Name"
+              value={signinUsername}
+              onChange={(e) => setSigninUsername(e.target.value)}
+              required
+            />
+            <input
+              type="password"
+              id="signinpassword"
+              name="signinpassword"
+              placeholder="Password"
+              value={signinPassword}
+              onChange={(e) => setSigninPassword(e.target.value)}
+              minLength={8}
+              required
+            />
+            <button id="signin-button" type="submit">
+              SignIn
+            </button>
+          </form>
+        </section>
+
+        <section className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-left">
+              <h1>Welcome Back!</h1>
+              <p>
+                Log in to access the admin dashboard and manage your employee
+                data efficiently.
+              </p>
+              <button onClick={() => setIsSignUp(false)} id="signIn">
+                Sign In
+              </button>
+            </div>
+            <div className="overlay-right">
+              <h1>Hello!</h1>
+              <p>
+                Join the admin community and start building efficient employee
+                management systems today.
+              </p>
+              <button onClick={() => setIsSignUp(true)} id="signUp">
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default Login;
